ec46a8c9aef7340b87a531ba8fb6a7b1ec1d2e43
Elizeu Elieber Fachini <elizeuelieber@live.com>
2018-11-24 14:58:35 -0200

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Models/Pedido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Pedido
+    {
+        [Key]
+        public int idPedido { get; set; }
+        public String NomeMedicamento { get; set; }
+        public String QtdeMedicamento { get; set; }
+        public String CodCliente { get; set; }
+        public String DataEntrega { get; set; }
+
+    }
+}
---
+++ WebMVC1/Models/PedidoMVC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebMVC1.Models
+{
+    public class PedidoMVC
+    {
+        [Key]
+        public int idPedido { get; set; }
+        [MinLength(3)]
+        [MaxLength(50)]
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Este campo é Obrigatório")]
+        public string NomeMedicamento { get; set; }
+        [Display(Name = "Quantidade")]
+        [Required(ErrorMessage = "Este campo é Obrigatório")]
+        public string QtdeMedicamento { get; set; }
+        [Required(ErrorMessage = "Este campo é Obrigatório")]
+        [Display(Name = "Codigo do cliente")]
+        [RegularExpression("^[0-9]*$")]
+        [StringLength(1, MinimumLength = 5)]
+        public string CodCliente { get; set; }
+        [Required(ErrorMessage = "Este campo é Obrigatório")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de entrega")]
+        public string DataEntrega { get; set; }
+    }
+}
